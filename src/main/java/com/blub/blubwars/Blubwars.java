2a273f3559e277f72a8927ccfd7c4636aa048a40
Ja90n <81578391+Ja90n@users.noreply.github.com>
2022-02-23 17:04:44 +0100

    the cat dies and respawns

--- src/main/java/com/blub/blubwars/Blubwars.java
+++ src/main/java/com/blub/blubwars/Blubwars.java
@@ -2,6 +2,7 @@ package com.blub.blubwars;
 
 import com.blub.blubwars.commands.blubwarsCommand;
 import com.blub.blubwars.commands.blubwarsTabCompleter;
+import com.blub.blubwars.events.entityDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Blubwars extends JavaPlugin {
@@ -13,6 +14,9 @@ public final class Blubwars extends JavaPlugin {
         // Get command
         getCommand("blubwars").setExecutor(new blubwarsCommand(this));
         getCommand("blubwars").setTabCompleter(new blubwarsTabCompleter(this));
+
+        // Get event
+        getServer().getPluginManager().registerEvents(new entityDeathEvent(this), this);
 
     }
 
--- src/main/java/com/blub/blubwars/commands/blubwarsCommand.java
+++ src/main/java/com/blub/blubwars/commands/blubwarsCommand.java
@@ -1,9 +1,15 @@
 package com.blub.blubwars.commands;
 
 import com.blub.blubwars.Blubwars;
+import org.bukkit.ChatColor;
+import org.bukkit.DyeColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.AnimalTamer;
+import org.bukkit.entity.Cat;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 
 public class blubwarsCommand implements CommandExecutor {
 
@@ -14,9 +20,20 @@ public class blubwarsCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-
-
-
+        if (sender instanceof Player){
+            Player p = (Player) sender;
+            if (args.length == 1){
+                switch (args[0]){
+                    case "cat":
+                        Cat cat = (Cat) p.getWorld().spawnEntity(p.getLocation(), EntityType.CAT);
+                        cat.setCatType(Cat.Type.BLACK);
+                        cat.setCollarColor(DyeColor.PINK);
+                        cat.setCustomName(ChatColor.LIGHT_PURPLE + "Pink cat: " + ChatColor.BLUE + "9 lives");
+                        cat.setOwner(p);
+                        break;
+                }
+            }
+        }
         return false;
     }
 }
