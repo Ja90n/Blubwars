406455cec1f0998f6bf2fe6c9362974de7c4132c
Alexander van Dam <alexiaan.vd@hotmail.com>
2022-03-06 15:26:20 +0100

    First try to remove the spaghetti.

--- src/main/java/com/blub/blubwars/command/ArenaCommand.java
+++ src/main/java/com/blub/blubwars/command/ArenaCommand.java
@@ -9,10 +9,15 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Locale;
 
 public class ArenaCommand implements CommandExecutor {
 
     private Blubwars blubwars;
+    private Player player;
+
     public ArenaCommand(Blubwars blubwars) {
         this.blubwars = blubwars;
     }
@@ -20,86 +25,130 @@ public class ArenaCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
+        boolean bool = false;
         if (sender instanceof Player) {
-            Player p = (Player) sender;
-            if (args.length == 1 && args[0].equalsIgnoreCase("list")) {
-                p.sendMessage(ChatColor.BLUE + "These are the avalible arenas:");
-                for (Arena arena : blubwars.getArenaManager().getArenas()) {
-                    p.sendMessage(ChatColor.BLUE + "- " + arena.getId() + "(" + arena.getState().name() + ")");
-                }
-            } else if (args.length == 1 && args[0].equalsIgnoreCase("leave")) {
-                Arena arena = blubwars.getArenaManager().getArena(p);
-                if (arena != null) {
-                    p.sendMessage(ChatColor.BLUE + "You have left the arena!");
-                    arena.removePlayer(p);
-                } else {
-                    p.sendMessage(ChatColor.RED + "You are not in a arena");
-                }
-            } else if (args.length == 2 && args[0].equalsIgnoreCase("join")) {
-                if (blubwars.getArenaManager().getArena(p) != null) {
-                    p.sendMessage(ChatColor.RED + "You are already in a arena!");
-                    return false;
-                }
-                int id;
-                try {
-                    id = Integer.parseInt(args[1]);
-                } catch (NumberFormatException e) {
-                    p.sendMessage(ChatColor.RED + "You need to fill in a number!");
-                    return false;
-                }
-                if (id >= 0 && id < blubwars.getArenaManager().getArenas().size()) {
-                    Arena arena = blubwars.getArenaManager().getArena(id);
-                    if (arena.getState().equals(GameState.RECRUITING) || arena.getState().equals(GameState.COUNTDOWN)) {
-                        if (arena.getCanJoin()){
-                            p.sendMessage(ChatColor.BLUE + "You are now playing in arena " + arena.getId() + "!");
-                            arena.addPlayer(p);
-                        } else {
-                            p.sendMessage(ChatColor.RED + "You can't join right now. Map still loading!");
-                        }
-                    } else {
-                        p.sendMessage(ChatColor.RED + "Game is already active!");
-                    }
-                } else {
-                    p.sendMessage(ChatColor.RED + "This arena doesn't exist");
-                }
-            } else if (args.length == 1 && args[0].equalsIgnoreCase("team")) {
+            Player player = (Player) sender;
+            this.player = player;
+            String message = args[0].toLowerCase(Locale.ROOT);
+            bool = handleCommand(message, args);
+        }
+        //It wil be always false? Why??
+        return bool;
+    }
 
-                Arena arena = blubwars.getArenaManager().getArena(p);
-                if (arena != null) {
-                    if (!(arena.getState() == GameState.LIVE)) {
-                        new TeamGui(arena, p);
-                    } else {
-                        p.sendMessage(ChatColor.RED + "You can not use this while the game is live");
-                    }
-                } else {
-                    p.sendMessage(ChatColor.RED + "You are not in a arena");
-                }
-
-
-            } else if (args.length == 2 && args[0].equalsIgnoreCase("forcestop")) {
-                int id;
-                try {
-                    id = Integer.parseInt(args[1]);
-                } catch (NumberFormatException e) {
-                    p.sendMessage(ChatColor.RED + "You need to fill in a number!");
-                    return false;
-                }
-                if (id >= 0 && id < blubwars.getArenaManager().getArenas().size()) {
-                    Arena arena = blubwars.getArenaManager().getArena(id);
-                    if (arena.getState().equals(GameState.LIVE)) {
-                        arena.reset();
-                        p.sendMessage(ChatColor.BLUE + "You have force stopped " + id);
-                    } else {
-                        p.sendMessage(ChatColor.RED + "Game is not active!");
-                    }
-                } else {
-                    p.sendMessage(ChatColor.RED + "This arena doesn't exist");
-                }
-            } else {
-                p.sendMessage(ChatColor.RED + "Invalid usage");
-            }
+    private boolean handleCommand(String message, String[] args) {
+        if (message == "list") {
+            getAllAreanasMessage();
 
         }
+        if (message == "leave") {
+            removePlayer();
+        }
+        if (message == "join") {
+            joinCommand(message, args);
+
+        }
+        if (message == "team") {
+            teamCommando(message, args);
+
+
+        }
+        if (message == "forcestop") {
+            stopCommand(message, args);
+        }
+        else {
+            sendMessage( ChatColor.RED + "Invalid usage");
+        }
+
         return false;
+    }
+
+    private boolean stopCommand(String message, String args[]) {
+        int id;
+        try {
+            id = Integer.parseInt(args[1]);
+        } catch (NumberFormatException e) {
+            sendMessage( ChatColor.RED + "You need to fill in a number!");
+            return false;
+        }
+        if (id >= 0 && id < blubwars.getArenaManager().getArenas().size()) {
+            Arena arena = blubwars.getArenaManager().getArena(id);
+            if (arena.getState().equals(GameState.LIVE)) {
+                arena.reset();
+                sendMessage( ChatColor.BLUE + "You have force stopped " + id);
+            } else {
+                sendMessage( ChatColor.RED + "Game is not active!");
+            }
+        } else {
+            sendMessage( ChatColor.RED + "This arena doesn't exist");
+        }
+
+        return false;
+    }
+
+    private boolean joinCommand(String message, String args[]) {
+        if (blubwars.getArenaManager().getArena(player) != null) {
+            sendMessage( ChatColor.RED + "You are already in a arena!");
+            return false;
+        }
+        int id;
+        try {
+            id = Integer.parseInt(args[1]);
+        } catch (NumberFormatException e) {
+            sendMessage( ChatColor.RED + "You need to fill in a number!");
+            return false;
+        }
+        if (id >= 0 && id < blubwars.getArenaManager().getArenas().size()) {
+            Arena arena = blubwars.getArenaManager().getArena(id);
+            if (arena.getState().equals(GameState.RECRUITING) || arena.getState().equals(GameState.COUNTDOWN)) {
+                if (arena.getCanJoin()) {
+                    sendMessage( ChatColor.BLUE + "You are now playing in arena " + arena.getId() + "!");
+                    arena.addPlayer(player);
+                } else {
+                    sendMessage( ChatColor.RED + "You can't join right now. Map still loading!");
+                }
+            } else {
+                sendMessage( ChatColor.RED + "Game is already active!");
+            }
+        } else {
+            sendMessage( ChatColor.RED + "This arena doesn't exist");
+        }
+        return false;
+    }
+
+    private void teamCommando(String message, String args[]) {
+        Arena arena = blubwars.getArenaManager().getArena(player);
+        if (arena != null) {
+            if (!(arena.getState() == GameState.LIVE)) {
+                new TeamGui(arena, player);
+            } else {
+                sendMessage( ChatColor.RED + "You can not use this while the game is live");
+            }
+        } else {
+            sendMessage( ChatColor.RED + "You are not in a arena");
+        }
+    }
+
+    private void removePlayer() {
+        Arena arena = blubwars.getArenaManager().getArena(player);
+        if (arena == null) {
+            sendMessage( ChatColor.RED + "You are not in a arena");
+        } else {
+            sendMessage( ChatColor.BLUE + "You have left the arena!");
+            arena.removePlayer(player);
+        }
+    }
+
+    private void sendMessage(@NotNull String message) {
+        player.sendMessage(message);
+    }
+
+    private void getAllAreanasMessage() {
+        sendMessage(ChatColor.BLUE + "These are the avalible arenas:");
+
+        for (Arena arena : blubwars.getArenaManager().getArenas()) {
+            player.sendMessage(ChatColor.BLUE + "- " + arena.getId() + "(" + arena.getState().name() + ")");
+        }
+
     }
 }
